29563b405118c57a24aa2f889e81fe84d93ae3ae
Simon Chauvin <contact@simonchauvin.com>
2015-11-06 17:42:32 +0100

    Single input behaviour.

---
+++ Assets/Scripts/Navigation/SingleInputBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingleInputBehaviour : NavigationBehaviour
+{
+    public char characterToEnter;
+
+	
+	public override void Start ()
+    {
+        base.Start();
+
+	}
+
+
+    public override void Update()
+    {
+        base.Update();
+
+	}
+
+    public override void receiveInput(string input)
+    {
+        if (input.Equals(characterToEnter.ToString()))
+        {
+            navigateToTarget();
+        }
+    }
+}
