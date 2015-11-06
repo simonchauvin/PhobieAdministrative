3ac8f07a0de636afed4216fd4166f0e71e8a200b
Simon Chauvin <contact@simonchauvin.com>
2015-11-06 22:28:13 +0100

    Multiple input behaviour.

---
+++ Assets/Scripts/Navigation/MultipleInputBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MultipleInputBehaviour : NavigationBehaviour
+{
+    public string charactersToEnter;
+
+    private List<char> charactersEntered;
+
+
+    public override void Start()
+    {
+        base.Start();
+
+        charactersEntered = new List<char>();
+    }
+
+
+    public override void Update()
+    {
+        base.Update();
+
+        
+    }
+
+    public override void activate()
+    {
+        base.activate();
+
+        charactersEntered = new List<char>();
+    }
+
+    public override void receiveInput(string input)
+    {
+        charactersEntered.Add(char.Parse(input));
+        if (charactersEntered.Count >= charactersToEnter.Length)
+        {
+            bool correctInput = true;
+            for (int i = 0; i < charactersToEnter.Length; i++)
+            {
+                if (!charactersEntered[charactersEntered.Count - charactersToEnter.Length + i].Equals(charactersToEnter[i]))
+                {
+                    correctInput = false;
+                }
+            }
+            if (correctInput)
+            {
+                navigateToTarget();
+            }
+        }
+    }
+}
