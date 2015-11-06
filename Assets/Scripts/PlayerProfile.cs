f4c5da312f25db2e54e1a896ce4f14b8b9881c22
Simon Chauvin <contact@simonchauvin.com>
2015-11-07 00:47:36 +0100

    Player profile.

---
+++ Assets/Scripts/PlayerProfile.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerProfile : MonoBehaviour
+{
+    public int id { get; set; }
+    public bool male { get; set; }
+    public bool female { get; set; }
+    public bool single { get; set; }
+    public bool married { get; set; }
+    public int dept { get; set; }
+    public int age { get; set; }
+    public int height { get; set; }
+    public int weight { get; set; }
+	
+
+	void Start ()
+    {
+        id = Random.Range(100000000, 999999999);
+	}
+	
+	
+	void Update ()
+    {
+	    
+	}
+}
