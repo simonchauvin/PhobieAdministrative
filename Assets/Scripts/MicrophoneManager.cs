5a11b8d2a1efa5f1d0c5b081e3efc36bd13c9b93
eskivor <pierre.stempin@gmail.com>
2015-11-06 17:50:19 +0100

    micro ok

--- Assets/Scripts/MicrophoneManager.cs
+++ Assets/Scripts/MicrophoneManager.cs
@@ -1,11 +1,45 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
+[RequireComponent (typeof (AudioSource))]
 public class MicrophoneManager : MonoBehaviour 
 {
+	public Text microphoneText;
+	public int duration;
+	public int frequency;
+
+	//Détermine si ça récrit par dessus à la fin
+	public bool loop;
+
+	private ButtonCall buttonCall;
+	private AudioSource audioSource;
+
 	void Start ()
 	{
+		audioSource = GetComponent<AudioSource>();
+
+		microphoneText.text = null;
+		//Debug.Log("Name: " + device);
 		foreach (string device in Microphone.devices) 
-			Debug.Log("Name: " + device);
+			microphoneText.text = microphoneText.text + "Name: " + device;
+
+		if (Microphone.devices.Length == 1)
+			microphoneText.text = "Micro " + Microphone.devices[0];
+	}
+
+	public void RecordMicro ()
+	{
+		audioSource.clip = Microphone.Start ("Built-in Microphone", loop, duration, frequency);
+	}
+
+	public void StopMicro ()
+	{
+		Microphone.End ("Built-in Microphone");
+	}
+
+	public void PlayMicro ()
+	{
+		audioSource.PlayOneShot (audioSource.clip);
 	}
 }
