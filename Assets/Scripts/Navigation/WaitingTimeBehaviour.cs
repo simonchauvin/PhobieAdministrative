3e326dd286d0a8e4a29dc023c4dad2c47f243078
Simon Chauvin <contact@simonchauvin.com>
2015-11-06 17:42:44 +0100

    Waiting time behaviour.

---
+++ Assets/Scripts/Navigation/WaitingTimeBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaitingTimeBehaviour : NavigationBehaviour
+{
+    // PUBLIC
+    public float timeToWait;
+
+    // PRIVATE
+    private float timer;
+
+
+	public override void Start ()
+    {
+        base.Start();
+
+        timer = 0f;
+	}
+
+
+    public override void Update()
+    {
+        base.Update();
+        
+        if (isActive)
+        {
+            timer += Time.deltaTime;
+            if (timer >= timeToWait)
+            {
+                navigateToTarget();
+            }
+        }
+	}
+
+    public override void activate ()
+    {
+        base.activate();
+
+        timer = 0f;
+    }
+
+    public override void receiveInput(string input)
+    {
+
+    }
+}
