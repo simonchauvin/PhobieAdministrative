1fc17c1fe8c8cf2def79152eecc7c0d14baf9cff
gael <kledii@hotmail.com>
2015-11-06 12:08:03 +0100

    NavigationBehaviour & NavigationState

--- Assets/Scripts/Navigation/NavigationBehaviour.cs
+++ Assets/Scripts/Navigation/NavigationBehaviour.cs
@@ -3,13 +3,92 @@ using System.Collections;
 
 public class NavigationBehaviour : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	/// <summary>
+	/// parent navigation.
+	/// </summary>
+	private NavigationState parentNavigationState;
+
+	/// <summary>
+	/// target navigation.
+	/// </summary>
+	public NavigationState targetNavigationState;
+
+	/// <summary>
+	/// The line renderer.
+	/// </summary>
+	private LineRenderer lineRenderer;
+
+	/// <summary>
+	/// The is active.
+	/// </summary>
+	public bool isActive {get; private set;}
+
+	/// <summary>
+	/// The color of the line.
+	/// </summary>
+	public Color lineColor;
+
+	/// <summary>
+	/// Init
+	/// </summary>
+	void Start()
+	{
+		parentNavigationState = GetComponent<NavigationState>();
+		if(parentNavigationState == null)
+			Debug.LogError("A navigation state must be attached to " + name + " for the navigationbehaviour to work.");
+
+		if(targetNavigationState == null)
+			Debug.LogError("A navigation state target must be specified.");
+
+
+		lineRenderer = GetComponent<LineRenderer>();
+		if(lineRenderer == null)
+			Debug.LogError("You must attach a lineRenderer to a NaviationBehaviour.");
+
+		lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+		lineRenderer.SetColors(lineColor, lineColor);
+		lineRenderer.SetWidth(0.2F, 0.2F);
+		lineRenderer.SetVertexCount(2);
+			
+	}
+
+	void Update()
+	{
+		//updateLineRenderer();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	/// <summary>
+	/// Updates the line renderer.
+	/// </summary>
+	public void updateLineRenderer()
+	{
+		lineRenderer.SetPosition(0, transform.position);
+		lineRenderer.SetPosition(1, targetNavigationState.transform.position);
 	}
+
+	/// <summary>
+	/// Navigates to target.
+	/// </summary>
+	public void navigateToTarget()
+	{
+		parentNavigationState.deactivate();
+		targetNavigationState.activate();
+	}
+
+	/// <summary>
+	/// Activate this instance.
+	/// </summary>
+	public void activate()
+	{
+		isActive = true;
+	}
+
+	/// <summary>
+	/// Deactivate this instance.
+	/// </summary>
+	public void deactivate()
+	{
+		isActive = false;
+	}
+
 }
--- Assets/Scripts/Navigation/NavigationState.cs
+++ Assets/Scripts/Navigation/NavigationState.cs
@@ -1,18 +1,84 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class NavigationState : MonoBehaviour
 {
+	/// <summary>
+	/// The navigation states count.
+	/// </summary>
+	public static int navigationStatesCount;
 
-	
-	void Start ()
-    {
-	
+	/// <summary>
+	/// Gets the identifier.
+	/// </summary>
+	/// <value>The identifier.</value>
+	public int id {get; private set;}
+
+	/// <summary>
+	/// The audio.
+	/// </summary>
+	public AudioClip audio;
+
+	/// <summary>
+	/// The navigation behaviours.
+	/// </summary>
+	private NavigationBehaviour[] navigationBehaviours;
+
+	/// <summary>
+	/// The is active.
+	/// </summary>
+	public bool isActive {get; private set;}
+
+	/// <summary>
+	/// Init.
+	/// </summary>
+	void Awake()
+	{
+		//Get attached navigation behaviours
+		navigationBehaviours = GetComponents<NavigationBehaviour>();
+
+		//Set Id
+		id = navigationStatesCount;
+		navigationStatesCount ++;
 	}
-	
-	
-	void Update ()
-    {
-	
+
+
+
+	/// <summary>
+	/// Activate this instance.
+	/// </summary>
+	public void activate()
+	{
+		isActive = true;
+		//Activate every attached NavigationBehaviour
+		foreach(NavigationBehaviour navigationBehaviour in navigationBehaviours)
+		{
+			navigationBehaviour.activate();
+		}
+
+		//Play Audio
 	}
+
+
+	/// <summary>
+	/// Deactivate this instance.
+	/// </summary>
+	public void deactivate()
+	{
+		isActive = false;
+		//Deactivate every attached NavigationBehaviour
+		foreach(NavigationBehaviour navigationBehaviour in navigationBehaviours)
+		{
+			navigationBehaviour.deactivate();
+		}
+
+		//Stop Audio
+
+		//
+		NavigationManager.instance.switchState(this);
+	}
+
+
+
 }
