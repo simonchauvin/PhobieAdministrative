40741844a38ba8cb233a8b5ae62ef4cc0354fb02
eskivor <pierre.stempin@gmail.com>
2015-11-07 12:03:51 +0100

    ajout des crédits

---
+++ Assets/Scripts/Buttons/ButtonCredits.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonCredits : MonoBehaviour 
+{
+	public GameObject creditsUI;
+	private bool isInCredits;
+
+	void Start ()
+	{
+		SetCreditsWindow ();
+	}
+
+	public void Credits ()
+	{
+		isInCredits = !isInCredits;
+		SetCreditsWindow ();
+	}
+
+	private void SetCreditsWindow ()
+	{
+		creditsUI.SetActive (isInCredits);
+	}
+}
