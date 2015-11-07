5a16fde8cb61fb853e3dfb93c7252a4962c18d99
gael <kledii@hotmail.com>
2015-11-07 01:32:04 +0100

    microphoneManager fix

--- Assets/Scripts/Micro/MicrophoneManager.cs
+++ Assets/Scripts/Micro/MicrophoneManager.cs
@@ -2,66 +2,67 @@
 using UnityEngine.UI;
 using System.Collections;
 
-[RequireComponent (typeof (AudioSource))]
-public class MicrophoneManager : MonoBehaviour 
+[RequireComponent(typeof(AudioSource))]
+public class MicrophoneManager : MonoBehaviour
 {
-	public Text microphoneText;
-	public int duration;
-	public int frequency;
+    public Text microphoneText;
+    public int duration;
+    public int frequency;
 
-	//Détermine si ça récrit par dessus à la fin
-	public bool loop;
+    //Détermine si ça récrit par dessus à la fin
+    public bool loop;
 
-	private ButtonCall buttonCall;
-	private AudioSource audioSource;
+    private ButtonCall buttonCall;
+    private AudioSource audioSource;
 
-	void Start ()
-	{
-		audioSource = GetComponent<AudioSource>();
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
 
-		microphoneText.text = null;
-		//Debug.Log("Name: " + device);
-		foreach (string device in Microphone.devices) 
-			microphoneText.text = microphoneText.text + "Name: " + device;
+        microphoneText.text = null;
+        //Debug.Log("Name: " + device);
+        foreach (string device in Microphone.devices)
+            microphoneText.text = microphoneText.text + "Name: " + device;
 
-		if (Microphone.devices.Length == 1)
-			microphoneText.text = "Micro " + Microphone.devices[0];
+        if (Microphone.devices.Length == 1)
+            microphoneText.text = "Micro " + Microphone.devices[0];
+#if UNITY_ANDROID
+        StartCoroutine("LoadAudioClip");
+#endif
+    }
 
-		StartCoroutine ("LoadAudioClip");
-	}
-
+#if UNITY_ANDROID
 	IEnumerator LoadAudioClip ()
 	{
-#if UNITY_ANDROID
 		string path = "file:///"+ Application.persistentDataPath + "/RecordedMessage.wav";
 		WWW www = new WWW (path);
 		yield return www;
 		AudioClip audioClipLoaded = www.GetAudioClip (false, false, AudioType.WAV);
 		audioSource.clip = audioClipLoaded;
+    }
 #endif
-	}
 
-	public void SaveAudioClip ()
-	{
-		SaveWav.Save ("RecordedMessage", audioSource.clip);
-	}
+    public void SaveAudioClip()
+    {
+        SaveWav.Save("RecordedMessage", audioSource.clip);
+    }
 
-	public void RecordMicro ()
-	{
-		audioSource.clip = null;
-		audioSource.clip = Microphone.Start ("Built-in Microphone", loop, duration, frequency);
-	}
+    public void RecordMicro()
+    {
+        audioSource.clip = null;
+        audioSource.clip = Microphone.Start("Built-in Microphone", loop, duration, frequency);
+    }
 
-	public void StopMicro ()
-	{
-		Microphone.End ("Built-in Microphone");
-		SaveAudioClip ();
-	}
+    public void StopMicro()
+    {
+        Microphone.End("Built-in Microphone");
+        SaveAudioClip();
+    }
 
-	public void PlayMicro ()
-	{
-		StopMicro ();
-		audioSource.Stop ();
-		audioSource.PlayOneShot (audioSource.clip);
-	}
+    public void PlayMicro()
+    {
+        StopMicro();
+        audioSource.Stop();
+        audioSource.PlayOneShot(audioSource.clip);
+    }
 }
